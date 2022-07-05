3f679fc97a3946ee006aa74e0b1407e3daf22511
Gabriel <gabriellopesbusiness@gmail.com>
2022-07-05 20:55:02 -0300

    chore: move client side to its own repository

--- server/src/database/sequelize.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-const { DB_URL } = require("../../config");
-
-const sequelize = new Sequelize(DB_URL, {
-    logging: false
-});
-
-module.exports = sequelize;
